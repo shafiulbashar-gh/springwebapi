bbf7c5b6d3bf683e5c9a89d8eff01084ebc22f21
Shafiul Bashar <shafiulbashar201@gmail.com>
2024-03-10 20:14:03 +0100

    Refactoring

--- src/main/java/com/example/SampleService/Models/Quantity.java
+++
@@ -1,8 +0,0 @@
-package com.example.SampleService.Models;
-
-public class Quantity {
-
-
-
-
-}
--- src/main/java/com/example/SampleService/Products/Interface/IDBConnector.java
+++ src/main/java/com/example/SampleService/Products/Interface/IDBConnector.java
@@ -1,11 +1,10 @@
 package com.example.SampleService.Repository.Interface;
 
-import com.example.SampleService.Config.ApplicationConfigurations;
-import com.example.SampleService.Models.Product;
+import com.example.SampleService.Products.Models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface IDBConnector extends JpaRepository<Product, Long> {
 
 
